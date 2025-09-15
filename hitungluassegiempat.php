bf79052639bf739c9eebbbb240dff1530ed26fd5
yulia534 <yulia100506@gmail.com>
2025-09-15 15:21:11 +0700

    first commit

---
+++ hitungluassegiempat.php
@@ -0,0 +1,11 @@
+<?php
+echo "<h1>menghitung luas segi empat</h1>";
+$sisi = 50;
+$sisi = 40;
+$luas = $sisi * $sisi;
+echo "sisi : $sisi";
+echo "<br>";
+echo "sisi : $sisi";
+echo "<br>";
+echo  "luas segi empat : $luas";
+?>
