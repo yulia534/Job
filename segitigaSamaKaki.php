35361663287ad734d3b9df937ffeec54d1fbccb9
yulia534 <yulia100506@gmail.com>
2025-09-15 15:22:35 +0700

    first commit

---
+++ segitigaSamaKaki.php
@@ -0,0 +1,20 @@
+<?php
+echo "<h1>menghitung segitiga sama kaki</h1>";
+$alas = 15;
+$tinggi = 20;
+$sisi = 30;
+$sisi = 35;
+$luas = 2 * $alas * $tinggi;
+$keliling = 2 * $sisi + $sisi + $alas;
+echo "sisi : $sisi";
+echo "<br>";
+echo "sisi : $sisi";
+echo "<br>";
+echo "alas : $alas";
+echo "<br>";
+echo "tinggi : $tinggi";
+echo "<br>";
+echo "Luas dari Segitiga Sama Kaki = $luas";
+echo "<br>";
+echo "keliling dari segitiga sama kaki = $keliling";
+?>
